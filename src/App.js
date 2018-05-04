55b5349f54aa5a9a9b9a38032cca96aa3e1d3db9
mkinfrared <mkinfrared@gmail.com>
2018-05-04 13:56:14 -0600

    finished navbar and routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {Route} from 'react-router-dom';
 import NavBar from './components/NavBar/nav-bar';
+import Router from './components/Router/router';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<NavBar/>
+				{Router}
 			</div>
 		);
 	}
--- src/components/NavBar/nav-bar.js
+++ src/components/NavBar/nav-bar.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import './nav-bar.css';
 
 export default function NavBar() {
     return(
     	<div key="NavBar" className="nav-bar">
 		   <div className="nav-bar-logo">
-			   <i className="fab fa-app-store"></i>
-			   <p>shelfie</p>
-			   {/*<Link><button>Dashboard</button></Link>*/}
+			   <Link to="/"><i className="fab fa-app-store"></i>
+			   <p>shelfie</p></Link>
 		   </div>
+		    <ul>
+			    <li><Link to="/dashboard">Dashboard</Link></li>
+			    <li><Link to="/add-inventory">Add inventory</Link></li>
+		    </ul>
 	    </div>
     )
 }
